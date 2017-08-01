593f818f64decc0bdc07537c9b59f5fe453d4a6d
maqg <maquangang@163.com>
2017-08-01 10:45:31 +0800

    maqg add playground directory

--- src/api/route.go
+++ src/api/route.go
@@ -22,22 +22,16 @@ func (api *Api) ApiRouter() *gin.Engine {
 	LoadApiConfig(BaseDir)
 
 	//router.LoadHTMLGlob(BaseDir + "frontend/apitest/templates/*.html")
-	router.LoadHTMLFiles(BaseDir+"frontend/apitest/templates/apitest.html",
-		BaseDir+"frontend/ng/index.html")
+	//router.LoadHTMLFiles(BaseDir+"frontend/apitest/templates/apitest.html",
+	//	BaseDir+"frontend/ng/index.html")
 
 	//router.LoadHTMLGlob(BaseDir + "frontend/ng/index.html")
 
-	router.Static("/static", BaseDir+"frontend/apitest/static")
-	router.Static("/ng", BaseDir+"frontend/ng")
-	router.Static("/app", BaseDir+"frontend/ng/app")
-
-	router.GET("/apitest", api.LoadApiTestPage)
-
-	router.GET("/", api.LoadNgPage)
+	//router.Static("/static", BaseDir+"frontend/apitest/static")
+	//router.GET("/apitest", api.LoadApiTestPage)
 
 	router.GET("/api/", api.ApiTest)
 	router.POST("/api/", api.ApiDispatch)
-	router.POST("/ng/api/", api.ApiDispatch)
 
 	return router
 }
--- src/main.go
+++ src/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"octlink/mirage/src/api"
-	"octlink/mirage/src/modules/account"
-	"octlink/mirage/src/modules/session"
-	"octlink/mirage/src/modules/user"
-	"octlink/mirage/src/modules/usergroup"
 	"octlink/mirage/src/utils"
 	"octlink/mirage/src/utils/octlog"
+)
+
+var (
+	port   int
+	addr   string
+	config string
+	cert   string
 )
 
 func initDebugConfig() {
@@ -17,21 +21,26 @@ func initDebugConfig() {
 }
 
 func initLogConfig() {
-
 	api.InitApiLog(octlog.DEBUG_LEVEL)
-
-	user.InitLog(octlog.DEBUG_LEVEL)
-	account.InitLog(octlog.DEBUG_LEVEL)
-	usergroup.InitLog(octlog.DEBUG_LEVEL)
-	session.InitLog(octlog.DEBUG_LEVEL)
 }
 
-const (
-	HTTP_SERVER = "0.0.0.0"
-	HTTP_PORT   = 8080
-)
+func init() {
+	flag.StringVar(&addr, "addr", "0.0.0.0", "listen addr")
+	flag.IntVar(&port, "port", 8000, "listen port")
+	flag.StringVar(&cert, "cert", "./cert/cert.crt", "cert file path")
+	flag.StringVar(&config, "config", "./config.json", "config file")
+}
+
+func usage() {
+	fmt.Println("  RVM Store of V" + utils.Version() + "\n")
+	fmt.Println("  ./rstore -port 8000 -config ./config.json\n")
+	flag.PrintDefaults()
+}
 
 func main() {
+
+	flag.Usage = usage
+	flag.Parse()
 
 	fmt.Println(utils.Version())
 
@@ -43,7 +52,7 @@ func main() {
 	}
 
 	server := &http.Server{
-		Addr:           fmt.Sprintf("%s:%d", HTTP_SERVER, HTTP_PORT),
+		Addr:           fmt.Sprintf("%s:%d", addr, port),
 		Handler:        api.ApiRouter(),
 		MaxHeaderBytes: 1 << 20,
 	}
