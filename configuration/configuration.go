f798f8108f9b32c84b6141d969cf892f797de50d
maqg <maquangang@163.com>
2017-08-03 14:05:46 +0800

    maqg for configuration parse

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,17 +1,45 @@
 package configuration
 
+import (
+	"io"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
 type Configuration struct {
 
 	// Version is the version which defines the format of the rest of the configuration
-	Version Version `yaml:"version"`
+	Version string `yaml:"version"`
 
 	// Loglevel is the level at which registry operations are logged.
-	Loglevel Loglevel `yaml:"loglevel,omitempty"`
+	Loglevel string `yaml:"loglevel,omitempty"`
 
 	// Debuglevel is the level for debugging.
-	Debuglevel Debuglevel `yaml:"debuglevel,omitempty"`
+	Debuglevel string `yaml:"debuglevel,omitempty"`
 
 	HTTP struct {
 		Addr string `yaml:"addr,omitempty"`
 	}
+}
+
+func ParseConfig(in []byte) (*Configuration, error) {
+
+	config := new(Configuration)
+
+	if err := yaml.Unmarshal(in, &config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func Parse(rd io.Reader) (*Configuration, error) {
+
+	in, err := ioutil.ReadAll(rd)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseConfig(in)
 }
--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -1,1 +1,27 @@
 package configuration
+
+import (
+	"fmt"
+	"testing"
+)
+
+var configYaml = `
+version: 0.1
+debuglevel: 5
+loglevel: 5
+http:
+  addr: :5000
+`
+
+func TestParseConfig(t *testing.T) {
+
+	config, err := ParseConfig([]byte(configYaml))
+	if err != nil {
+		fmt.Printf("parse config error\n")
+		return
+	}
+
+	fmt.Printf("version %s\n", config.Version)
+	fmt.Printf("debuglevel %s\n", config.Debuglevel)
+	fmt.Printf("loglevel %s\n", config.Loglevel)
+}
