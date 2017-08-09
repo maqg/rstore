74a8ba4aee3d62fc57a9edee24bc2f21d959e4b5
maqg <maquangang@163.com>
2017-08-09 14:45:19 +0800

    add descriptors and routes for gorilla

--- playground/gorilla/main.go
+++ playground/gorilla/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"octlink/rstore/playground/gorilla/v1"
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
+	for k, v := range v1.RouteDescriptorsMap {
+		w.Write([]byte(k))
+		w.Write([]byte(v.Path))
+	}
+
 	w.Write([]byte("Gorilla!\n"))
 }
 
 func main() {
-	r := mux.NewRouter()
+	r := v1.NewRouters()
+
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", YourHandler)
 
