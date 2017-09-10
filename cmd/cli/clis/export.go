0867fe787815d00fac3779cbf7433b239979e28e
maqg <maquangang@163.com>
2017-09-10 23:33:29 +0800

    remove imports from mirage

--- cmd/cli/clis/export.go
+++ cmd/cli/clis/export.go
@@ -2,11 +2,11 @@ package clis
 
 import (
 	"fmt"
-	"octlink/mirage/src/utils/merrors"
 	"octlink/rstore/modules/blobsmanifest"
 	"octlink/rstore/modules/manifest"
 	"octlink/rstore/utils"
 	"octlink/rstore/utils/configuration"
+	"octlink/rstore/utils/merrors"
 
 	"github.com/spf13/cobra"
 )
